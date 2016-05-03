d0c9c27b76131543ca097dd53e01ad7b1e767e06
alphaville <p.sopasakis@gmail.com>
2016-05-03 19:48:46 +0200

    make > /dev/null in .travis.yml

--- scripts/install-suitesparse.sh
+++ scripts/install-suitesparse.sh
@@ -2,5 +2,5 @@ wget http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-4.4.6.tar.gz -O .
 tar xvf ./libs/SuiteSparse.tar.gz -C ./libs
 rm -rf ./libs/SuiteSparse.tar.gz
 cd ./libs/SuiteSparse
-make
+make > /dev/null
 cd ../../
