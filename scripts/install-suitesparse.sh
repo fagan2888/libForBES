0a9b3d19e4b1b7c69b83f8af0e8a256215560666
alphaville <p.sopasakis@gmail.com>
2016-05-03 19:03:59 +0200

    Modifying .travis.yml

--- scripts/install-suitesparse.sh
+++ scripts/install-suitesparse.sh
@@ -1,4 +1,3 @@
-cd ../
 wget http://faculty.cse.tamu.edu/davis/SuiteSparse/SuiteSparse-4.4.6.tar.gz -O ./libs/SuiteSparse.tar.gz
 tar xvf ./libs/SuiteSparse.tar.gz -C ./libs
 rm -rf ./libs/SuiteSparse.tar.gz
